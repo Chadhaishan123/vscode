2475884d462611a545fb879aee6fe9cc5b8144ae
João Moreno <joao.moreno@microsoft.com>
2023-03-19 22:09:01 +0100

    start with distro?

--- build/azure-pipelines/linux/scripts/install-remote-dependencies.sh
+++ build/azure-pipelines/linux/scripts/install-remote-dependencies.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
 set -e
 
-echo "Installing remote dependencies"
-(cd remote && rm -rf node_modules)
+echo "Installing distro dependencies"
 for i in {1..5}; do # try 5 times
-  yarn --verbose --cwd remote --frozen-lockfile --check-files && break
+  yarn --verbose --cwd .build/distro/npm --frozen-lockfile --check-files && break
   if [ $i -eq 3 ]; then
     echo "Yarn failed too many times" >&2
     exit 1
@@ -12,9 +11,11 @@ for i in {1..5}; do # try 5 times
   echo "Yarn failed $i, trying again..."
 done
 
-echo "Installing distro dependencies"
+
+echo "Installing remote dependencies"
+(cd remote && rm -rf node_modules)
 for i in {1..5}; do # try 5 times
-  yarn --verbose --cwd .build/distro/npm --frozen-lockfile --check-files && break
+  yarn --verbose --cwd remote --frozen-lockfile --check-files && break
   if [ $i -eq 3 ]; then
     echo "Yarn failed too many times" >&2
     exit 1
